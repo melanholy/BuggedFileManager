e11e9ac84cf6dc34b7963eb32e0d52530541ca95
admin <admin@ADMIN-PC>
2016-06-06 21:30:33 +0500

    комментарии

--- API/ClickPlace.cs
+++ API/ClickPlace.cs
@@ -1,5 +1,9 @@
 ﻿namespace API
 {
+    /// <summary>
+    /// Вспомогательный енум для IMenuItem, показывающий, где произошел 
+    /// клик, который вызвал открытие меню.
+    /// </summary>
     public enum ClickPlace
     {
         Folder,
--- API/IFileIcon.cs
+++ API/IFileIcon.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 namespace API
 {
+    /// <summary>
+    /// Иконка для файлов.
+    /// Как использовать: при необходимости отобразить новый файл нужно
+    /// по его расширению взять соответствующий IFileIcon и получить
+    /// изображение из указанного Uri.
+    /// </summary>
     public interface IFileIcon
     {
         List<string> Extensions { get; }
